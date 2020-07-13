1b129d3066d6490d12f3a69853e6670c11688400
Avililla <54501703+Avililla@users.noreply.github.com>
2020-07-13 21:21:16 +0200

    New update. Solved problems on load?

--- VORP-Inventory[Client-Server]/vorpinventory_sv/ItemDatabase.cs
+++ VORP-Inventory[Client-Server]/vorpinventory_sv/ItemDatabase.cs
@@ -15,79 +15,101 @@ namespace vorpinventory_sv
         public static Dictionary<string, Items> svItems = new Dictionary<string, Items>();
         public ItemDatabase()
         {
+            LoadDatabase();
+        }
+
+        private async void LoadDatabase()
+        {
+            await Delay(2000);
             Exports["ghmattimysql"].execute("SELECT * FROM items", new Action<dynamic>((result) =>
-             {
-                 if (result.Count == 0)
-                 {
-                     Debug.WriteLine("There`re no items in database");
-                 }
-                 else
-                 {
-                     items = result;
-                     foreach (dynamic item in items)
-                     {
-                         svItems.Add(item.item.ToString(), new Items(item.item, item.label, int.Parse(item.limit.ToString()), item.can_remove,item.type,item.usable));
-                     }
-                     Exports["ghmattimysql"].execute("SELECT identifier,inventory FROM characters", new Action<dynamic>((uinvento) =>
-                     {
-                         foreach (var userInventory in uinvento)
-                         {
-                             //Carga del inventario
-                             string user = userInventory.identifier.ToString();
-                             Dictionary<string, ItemClass> userinv = new Dictionary<string, ItemClass>();
-                             List<WeaponClass> userwep = new List<WeaponClass>();
-                             if (userInventory.inventory != null)
-                             {
-                                 dynamic thing = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(userInventory.inventory);
-                                 foreach (dynamic itemname in items)
-                                 {
-                                     if (thing[itemname.item.ToString()] != null)
-                                     {
-                                         ItemClass item = new ItemClass(int.Parse(thing[itemname.item.ToString()].ToString()), int.Parse(itemname.limit.ToString()),
-                                             itemname.label, itemname.item, itemname.type, itemname.usable, itemname.can_remove);
-                                         userinv.Add(itemname.item.ToString(), item);
-                                     }
-                                 }
-                                 usersInventory.Add(user, userinv);
-                             }
-                             else
-                             {
-                                 usersInventory.Add(user, userinv);
-                             }
-                         }
-                     }));
-                     Exports["ghmattimysql"].execute("SELECT * FROM loadout", new Action<dynamic>((weaponsinvento) =>
-                     {
-                         WeaponClass wp;
-                         foreach (var row in weaponsinvento)
-                         {
+            {
+                if (result.Count == 0)
+                {
+                    Debug.WriteLine("There`re no items in database");
+                }
+                else
+                {
+                    items = result;
+                    foreach (dynamic item in items)
+                    {
+                        svItems.Add(item.item.ToString(), new Items(item.item, item.label, int.Parse(item.limit.ToString()), item.can_remove, item.type, item.usable));
+                    }
+                    Exports["ghmattimysql"].execute("SELECT identifier,inventory FROM characters", new Action<dynamic>((uinvento) =>
+                    {
+                        if(uinvento.Count == 0)
+                        {
+                            Debug.WriteLine("No users inventory");
+                        }
+                        else
+                        {
+                            foreach (var userInventory in uinvento)
+                            {
+                                //Carga del inventario
+                                string user = userInventory.identifier.ToString();
+                                Dictionary<string, ItemClass> userinv = new Dictionary<string, ItemClass>();
+                                List<WeaponClass> userwep = new List<WeaponClass>();
+                                if (userInventory.inventory != null)
+                                {
+                                    dynamic thing = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(userInventory.inventory);
+                                    foreach (dynamic itemname in items)
+                                    {
+                                        if (thing[itemname.item.ToString()] != null)
+                                        {
+                                            ItemClass item = new ItemClass(int.Parse(thing[itemname.item.ToString()].ToString()), int.Parse(itemname.limit.ToString()),
+                                                itemname.label, itemname.item, itemname.type, itemname.usable, itemname.can_remove);
+                                            userinv.Add(itemname.item.ToString(), item);
+                                        }
+                                    }
+                                    usersInventory.Add(user, userinv);
+                                }
+                                else
+                                {
+                                    usersInventory.Add(user, userinv);
+                                }
+                            }
+                        }
+                        
+                    }));
+                    Exports["ghmattimysql"].execute("SELECT * FROM loadout", new Action<dynamic>((weaponsinvento) =>
+                    {
+                        if(weaponsinvento.Count == 0)
+                        {
+                            Debug.WriteLine("There is no loadout on server database");
+                        }
+                        else
+                        {
+                            WeaponClass wp;
+                            foreach (var row in weaponsinvento)
+                            {
 
-                             JObject ammo = Newtonsoft.Json.JsonConvert.DeserializeObject(row.ammo.ToString());
-                             JArray comp = Newtonsoft.Json.JsonConvert.DeserializeObject(row.components.ToString());
-                             Dictionary<string, int> amunition = new Dictionary<string, int>();
-                             List<string> components = new List<string>();
-                             foreach (JProperty ammos in ammo.Properties())
-                             {
-                                 //Debug.WriteLine(ammos.Name);
-                                 amunition.Add(ammos.Name, int.Parse(ammos.Value.ToString()));
-                             }
-                             foreach (JToken x in comp)
-                             {
-                                 components.Add(x.ToString());
-                             }
+                                JObject ammo = Newtonsoft.Json.JsonConvert.DeserializeObject(row.ammo.ToString());
+                                JArray comp = Newtonsoft.Json.JsonConvert.DeserializeObject(row.components.ToString());
+                                Dictionary<string, int> amunition = new Dictionary<string, int>();
+                                List<string> components = new List<string>();
+                                foreach (JProperty ammos in ammo.Properties())
+                                {
+                                    //Debug.WriteLine(ammos.Name);
+                                    amunition.Add(ammos.Name, int.Parse(ammos.Value.ToString()));
+                                }
+                                foreach (JToken x in comp)
+                                {
+                                    components.Add(x.ToString());
+                                }
 
-                             bool auused = false;
-                             if (row.used == 1)
-                             {
-                                 auused = true;
-                             }
-                             wp = new WeaponClass(int.Parse(row.id.ToString()), row.identifier.ToString(), row.name.ToString(), amunition, components, auused);
-                             userWeapons.Add(wp.getId(), wp);
-                         }
-                     }));
+                                bool auused = false;
+                                if (row.used == 1)
+                                {
+                                    auused = true;
+                                }
+                                wp = new WeaponClass(int.Parse(row.id.ToString()), row.identifier.ToString(), row.name.ToString(), amunition, components, auused);
+                                userWeapons.Add(wp.getId(), wp);
+                            }
+                        }
+                      
+                    }));
 
-                 }
-             }));
+                }
+            }));
         }
     }
 }
