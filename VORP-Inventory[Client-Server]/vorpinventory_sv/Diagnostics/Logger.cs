9e222de76c763bf13f6560513609f008e636807a
local9 <apjcookie1@hotmail.com>
2022-01-17 00:47:36 +0000

    Added console colours

--- VORP-Inventory[Client-Server]/vorpinventory_sv/Diagnostics/Logger.cs
+++ VORP-Inventory[Client-Server]/vorpinventory_sv/Diagnostics/Logger.cs
@@ -6,36 +6,43 @@ namespace VorpInventory.Diagnostics
     {
         public static void Info(string msg)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             WriteLine("INFO", msg);
         }
 
         public static void Success(string msg)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             WriteLine("SUCCESS", msg);
         }
 
         public static void Warn(string msg)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             WriteLine("WARN", msg);
         }
 
         public static void Error(string msg)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             WriteLine("ERROR", msg);
         }
 
         public static void Error(Exception ex, string msg = "")
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             WriteLine("ERROR", $"{msg}\r\n{ex}");
         }
 
         public static void Verbose(string msg)
         {
+            Console.ForegroundColor = ConsoleColor.Gray;
             WriteLine("VERBOSE", msg);
         }
 
         public static void Debug(string msg)
         {
+            Console.ForegroundColor = ConsoleColor.White;
             WriteLine("DEBUG", msg);
         }
 
@@ -50,6 +57,7 @@ namespace VorpInventory.Diagnostics
             {
                 Console.WriteLine(ex);
             }
+            Console.ResetColor();
         }
     }
 }
