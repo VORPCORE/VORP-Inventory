c9f1a2f34f8d7c05063417b1a5c97093c6a3b53f
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-03-04 20:05:35 +0000

    Delete nuistuff.lua
    merged to client

--- VORP-Inventory/Build/vorp_inventory/nuistuff.lua
+++
@@ -1,9 +0,0 @@
-RegisterNetEvent('syn:getnuistuff')
-AddEventHandler('syn:getnuistuff', function(x,y)
-	local nuistuff = x 
-	SendNUIMessage({
-		action = "changecheck",
-		check = nuistuff,
-		info = y,
-	})
-end)
