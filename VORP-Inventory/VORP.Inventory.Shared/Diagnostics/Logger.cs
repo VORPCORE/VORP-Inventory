328ec949ea1d13b34e0baf91cdc7bd5fccdd885d
local9 <apjcookie1@hotmail.com>
2022-04-09 22:16:37 +0100

    Logger added to shared

---
+++ VORP-Inventory/VORP.Inventory.Shared/Diagnostics/Logger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace VORP.Inventory.Shared.Diagnostics
+{
+    public static class Logger
+    {
+        static string _loggingLevel = GetResourceMetadata(GetCurrentResourceName(), "log_level", 0);
+
+        static bool ShowOutput(string level)
+        {
+            string lowercase = _loggingLevel.ToLower();
+            if (lowercase == "all") return true;
+            return (lowercase == level);
+        }
+
+        public static void Info(string msg)
+        {
+            if (ShowOutput("info"))
+                Format($"[INFO] {msg}");
+        }
+
+        public static void Trace(string msg)
+        {
+            if (ShowOutput("trace"))
+                Format($"[TRACE] {msg}");
+        }
+
+        public static void Warn(string msg)
+        {
+            if (ShowOutput("warn"))
+                Format($"[WARN] {msg}");
+        }
+
+        public static void Debug(string msg)
+        {
+            if (ShowOutput("debug"))
+                Format($"[DEBUG] {msg}");
+        }
+
+        public static void Error(string msg)
+        {
+            if (ShowOutput("error"))
+                Format($"[ERROR] {msg}");
+        }
+
+        public static void Error(Exception ex, string msg)
+        {
+            if (ShowOutput("error"))
+                Format($"[ERROR] {msg}\r\n{ex}");
+        }
+
+        static void Format(string msg)
+        {
+            CitizenFX.Core.Debug.WriteLine($"{msg}");
+        }
+    }
+}
