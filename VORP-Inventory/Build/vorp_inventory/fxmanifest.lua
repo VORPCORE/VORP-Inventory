41b36a9fc6f3568695d1bca75ac2410d11283158
local9 <apjcookie1@hotmail.com>
2022-04-09 21:28:53 +0100

    add config to files

--- VORP-Inventory/Build/vorp_inventory/fxmanifest.lua
+++ VORP-Inventory/Build/vorp_inventory/fxmanifest.lua
@@ -3,26 +3,11 @@ game 'rdr3'
 fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-
 ui_page 'html/ui.html'
 
+client_scripts {'config.lua', 'vorpinventory_cl.net.dll', 'client/client.lua'}
+server_scripts {'config.lua', 'vorpinventory_sv.net.dll', 'vorpInventoryAPI.lua', 'server/server.lua'}
 
-client_scripts{
-  'config.lua',
-  'vorpinventory_cl.net.dll',
-  'client/client.lua'
-}
-server_scripts {
-  'config.lua',
-  'vorpinventory_sv.net.dll',
-  'vorpInventoryAPI.lua',
-  'server/server.lua'
-}
+server_exports {'vorp_inventoryApi'}
 
-
-server_exports{'vorp_inventoryApi'} 
-
-files{
-  'Newtonsoft.Json.dll',
-  'html/**/*'
-}
+files {'Newtonsoft.Json.dll', 'html/**/*', 'config.json'}
