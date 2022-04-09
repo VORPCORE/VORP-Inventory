8b4bbc209d83fd4a12b806f03e85326d2ce6d3c2
local9 <apjcookie1@hotmail.com>
2022-04-09 22:06:58 +0100

    Initial update for the Plugin Manager

--- VORP-Inventory/VORP.Inventory.Client/PluginManager.cs
+++ VORP-Inventory/VORP.Inventory.Client/PluginManager.cs
@@ -1,13 +1,35 @@
 ﻿global using static CitizenFX.Core.Native.API;
 using CitizenFX.Core;
+using System;
+using System.Threading.Tasks;
+using VORP.Inventory.Shared;
 
 namespace VorpInventory
 {
     public class PluginManager : BaseScript
     {
+        public static PluginManager Instance;
+
         public PluginManager()
         {
-            
+            Instance = this;
+
+            Config config = Configuration.Config;
+        }
+
+        public void AttachTickHandler(Func<Task> task)
+        {
+            Tick += task;
+        }
+
+        public void DetachTickHandler(Func<Task> task)
+        {
+            Tick -= task;
+        }
+
+        public void Hook(string eventName, Delegate @delegate)
+        {
+            EventHandlers[eventName] += @delegate;
         }
     }
 }
