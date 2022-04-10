af4df7b07d690bdcaac16b42d102cb967ed71a72
local9 <apjcookie1@hotmail.com>
2022-04-10 12:41:44 +0100

    update fx_version

--- VORP-Inventory/Build/vorp_inventory/fxmanifest.lua
+++ VORP-Inventory/Build/vorp_inventory/fxmanifest.lua
@@ -1,6 +1,6 @@
 game 'rdr3'
 
-fx_version 'adamant'
+fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 ui_page 'html/ui.html'
