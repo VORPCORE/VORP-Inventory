951e7d2251da17de25bf7a04c01bea637de0ab84
local9 <apjcookie1@hotmail.com>
2022-04-10 11:51:48 +0100

    Added NUI Message object

---
+++ VORP-Inventory/VORP.Inventory.Shared/Models/NuiMessage.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.Serialization;
+
+namespace VORP.Inventory.Shared.Models
+{
+    [DataContract]
+    public class NuiMessage
+    {
+        [DataMember(Name = "player")]
+        public int PlayerID { get; set; }
+
+        [DataMember(Name = "item")]
+        public string Item { get; set; }
+
+        [DataMember(Name = "what")]
+        public string Action { get; set; }
+
+        [DataMember(Name = "type")]
+        public string Type { get; set; }
+
+        [DataMember(Name = "hash")]
+        public string Hash { get; set; }
+
+        [DataMember(Name = "amount")]
+        public double Amount { get; set; }
+
+        [DataMember(Name = "id")]
+        public int ID { get; set; }
+
+        [DataMember(Name = "number")]
+        public int Number { get; set; }
+
+        [DataMember(Name = "horse")]
+        public int HorseID { get; set; }
+
+        [DataMember(Name = "wagon")]
+        public int WagonID { get; set; }
+
+        [DataMember(Name = "hideout")]
+        public int HideoutID { get; set; }
+
+        [DataMember(Name = "house")]
+        public int HouseID { get; set; }
+
+        [DataMember(Name = "clan")]
+        public int ClanID { get; set; }
+
+        [DataMember(Name = "steal")]
+        public int StealID { get; set; }
+
+        [DataMember(Name = "container")]
+        public int ContainerID { get; set; }
+    }
+}
