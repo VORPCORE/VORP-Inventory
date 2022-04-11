f21703293c81d2eec72ec1db6ea774160d276a98
local9 <apjcookie1@hotmail.com>
2022-04-11 22:35:18 +0100

    Added some RedM Classes for Prompts

---
+++ VORP-Inventory/VORP.Inventory.Client/RedM/PoolObject.cs
@@ -0,0 +1,40 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+namespace VORP.Inventory.Client.RedM
+{
+    public interface ISpatial
+    {
+        Vector3 Position { get; set; }
+        Vector3 Rotation { get; set; }
+    }
+
+    public interface IExistable
+    {
+        bool Exists();
+    }
+
+    public interface IDeletable : IExistable
+    {
+        void Delete();
+    }
+
+    public abstract class PoolObject : INativeValue, IDeletable
+    {
+        protected PoolObject(int handle)
+        {
+            Handle = handle;
+        }
+
+        public int Handle { get; protected set; }
+        public override ulong NativeValue
+        {
+            get { return (ulong)Handle; }
+            set { Handle = unchecked((int)value); }
+        }
+
+        public abstract bool Exists();
+
+        public abstract void Delete();
+    }
+}
