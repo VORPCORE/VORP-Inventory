cf39eb0ee4ff2cf581d5e5a3085ee1a2960c9136
local9 <apjcookie1@hotmail.com>
2022-04-12 01:41:21 +0100

    REMOVE 3D TEXT METHOD! 🎉

--- VORP-Inventory/VORP.Inventory.Client/Utils.cs
+++ VORP-Inventory/VORP.Inventory.Client/Utils.cs
@@ -95,23 +95,5 @@ namespace VORP.Inventory.Client
 
             return closestPlayers;
         }
-
-        // This should be replaced with prompts, solution requires designing.
-        public static void DrawText3D(Vector3 position, string text)
-        {
-            float _x = 0.0F;
-            float _y = 0.0F;
-            API.GetScreenCoordFromWorldCoord(position.X, position.Y, position.Z, ref _x, ref _y);
-            API.SetTextScale(0.35F, 0.35F);
-            API.SetTextFontForCurrentCommand(1);
-            API.SetTextColor(255, 255, 255, 215);
-            long str = Function.Call<long>(Hash._CREATE_VAR_STRING, 10, "LITERAL_STRING", text);
-            Function.Call((Hash)0xBE5261939FBECB8C, 1);
-            Function.Call((Hash)0xD79334A4BB99BAD1, str, _x, _y);
-            float factor = text.Length / 150.0F;
-            Function.Call((Hash)0xC9884ECADE94CB34, "generic_textures", "hud_menu_4a", _x, _y + 0.0125F, 0.015F + factor,
-                0.03F, 0.1F, 100, 1, 1, 190, 0);
-        }
-
     }
 }
