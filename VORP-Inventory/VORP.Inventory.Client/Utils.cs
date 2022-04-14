2d82523e2aedf60ecae2a9bc9ab93c0262d9585f
local9 <apjcookie1@hotmail.com>
2022-04-14 21:35:35 +0100

    Remove texture and change font for dev string

--- VORP-Inventory/VORP.Inventory.Client/Utils.cs
+++ VORP-Inventory/VORP.Inventory.Client/Utils.cs
@@ -111,11 +111,8 @@ namespace VORP.Inventory.Client
             API.SetTextFontForCurrentCommand(1);
             API.SetTextColor(255, 255, 255, 215);
             long str = Function.Call<long>(Hash._CREATE_VAR_STRING, 10, "LITERAL_STRING", text);
-            Function.Call((Hash)0xBE5261939FBECB8C, 1);
+            Function.Call((Hash)0xBE5261939FBECB8C, 18);
             Function.Call((Hash)0xD79334A4BB99BAD1, str, _x, _y);
-            float factor = text.Length / 150.0F;
-            Function.Call((Hash)0xC9884ECADE94CB34, "generic_textures", "hud_menu_4a", _x, _y + 0.0125F, 0.015F + factor,
-                0.03F, 0.1F, 100, 1, 1, 190, 0);
         }
 #endif
     }
